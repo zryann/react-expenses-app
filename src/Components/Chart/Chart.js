25f1f0f8a529d3fe2569547f839162f3a4bf6cef
Zryan Muhammed Jaafer <zryan.muhammed.jaafer@gmail.com>
2022-12-23 15:57:37 +0300

    Final version

--- src/Components/Chart/Chart.js
+++ src/Components/Chart/Chart.js
@@ -1,13 +1,15 @@
 import ChartBar from "./ChartBar";
 import "./Chart.css";
 const Chart = (props) => {
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMax = Math.max(...dataPointValues);
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
         <ChartBar
           key={dataPoint.id}
           value={dataPoint.value}
-          maxValue={null}
+          maxValue={totalMax}
           label={dataPoint.label}
         />
       ))}
--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 const Expenses = ({ data }) => {
   const [filteredYear, setFilteredYear] = useState("2020");
   const changeFilterHandler = (selectedYear) => {
@@ -18,6 +19,7 @@ const Expenses = ({ data }) => {
           selected={filteredYear}
           onChangeFilter={changeFilterHandler}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList expenses={filteredExpenses} />
       </Card>
     </div>
